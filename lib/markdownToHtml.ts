389b3a45f38b042e40fe1280ad0b2be608639507
Aman Mohla <amohla@atlassian.com>
2020-09-01 22:26:37 +1000

    Fix typescript import for shiki

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,7 +2,7 @@ import remark from 'remark';
 import remark2rehype from 'remark-rehype';
 import doc from 'rehype-document';
 import stringify from 'rehype-stringify';
-import shiki from 'rehype-shiki';
+const shiki = require('rehype-shiki');
 
 export default async function markdownToHtml(markdown: string) {
     const result = await remark()
