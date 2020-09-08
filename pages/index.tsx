d3de6a079d6a1b50bf6c7d8def0372e31c2c4542
Aman Mohla <amohla@atlassian.com>
2020-09-08 12:22:13 +1000

    Remove tailwindcss and integrate theme-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,79 @@
-import { getAllPosts } from '../lib/api'
-import Link from 'next/link'
-import DateFormatter from '../components/date-formater'
-import Post from '../types/post'
+// import { getAllPosts } from '../lib/api'
+// import Link from 'next/link'
+// import DateFormatter from '../components/date-formater'
+// import Post from '../types/post'
 
-type IndexPageProps = {
-  allPosts: Post[]
-}
+// type IndexPageProps = {
+//   allPosts: Post[]
+// }
 
 
 
-const Index = ({ allPosts }: IndexPageProps) => {
+// const Index = ({ allPosts }: IndexPageProps) => {
+//   return (
+//     <div className="max-w-2xl mx-auto px-6">
+//       <main className="mt-12 mb-24">
+//         <div className="flex flex-col items-center lg:flex-row">
+//           <img
+//             src="/assets/images/compressed-profile.jpg"
+//             className="w-32 h-32 lg:w-40 lg:h-40 rounded-full mb-12 lg:mb-0 border-2"
+//             alt="profile image Aman Prakash Mohla"
+//           />
+//           <header className="text-xl lg:pl-16">
+//             <div className="mb-2 font-bold">Hey there! 👋🏼</div>
+//             <div>
+//               I am Aman Prakash Mohla, a full stack developer based in Sydney, Australia.
+//               I love building web applications using Javascript and GoLang.
+//               I currently work with Atlassian and have previously co-founded Tablehero.
+//             </div>
+//           </header>
+//         </div>
+//       </main>
+//       <h2>Thoughts</h2>
+//       {allPosts.map((post, i) => {
+//         const postLink = `/blog/${post.slug}`;
+//         return (<article key={i} className="post-list-item">
+//           <a href={postLink} className="no-underline">
+//             <header className="post-title">{post.title}</header>
+//             <div className="post-excerpt">{post.excerpt}</div>
+//           </a>
+//         </article>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+
+// export default Index
+
+// export const getStaticProps = async () => {
+//   const allPosts = getAllPosts([
+//     'title',
+//     'date',
+//     'slug',
+//     'author',
+//     'coverImage',
+//     'excerpt',
+//     'draft',
+//   ])
+
+//   return {
+//     props: { allPosts },
+//   }
+// }
+
+import React from 'react'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../theme'
+
+const index = () => {
   return (
-    <div className="max-w-2xl mx-auto px-6">
-      <main className="mt-12 mb-24">
-        <div className="flex flex-col items-center lg:flex-row">
-          <img
-            src="/assets/images/compressed-profile.jpg"
-            className="w-32 h-32 lg:w-40 lg:h-40 rounded-full mb-12 lg:mb-0 border-2"
-            alt="profile image Aman Prakash Mohla"
-          />
-          <header className="text-xl lg:pl-16">
-            <div className="mb-2 font-bold">Hey there! 👋🏼</div>
-            <div>
-              I am Aman Prakash Mohla, a full stack developer based in Sydney, Australia.
-              I love building web applications using Javascript and GoLang.
-              I currently work with Atlassian and have previously co-founded Tablehero.
-            </div>
-          </header>
-        </div>
-      </main>
-      <h2>Thoughts</h2>
-      {allPosts.map((post, i) => {
-        const postLink = `/blog/${post.slug}`;
-        return (<article key={i} className="post-list-item">
-          <a href={postLink} className="no-underline">
-            <header className="post-title">{post.title}</header>
-            <div className="post-excerpt">{post.excerpt}</div>
-          </a>
-        </article>
-        )
-      })}
+    <ThemeProvider theme={theme}>
+      <div>
+        Hello World!
     </div>
+    </ThemeProvider>
   )
 }
 
-export default Index
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-    'draft',
-  ])
-
-  return {
-    props: { allPosts },
-  }
-}
+export default index
---
+++ theme.ts
@@ -0,0 +1,20 @@
+export default {
+    fonts: {
+        body:
+            'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+        heading: '"Avenir Next", sans-serif',
+        monospace: 'Menlo, monospace',
+    },
+    colors: {
+        text: '#091524',
+        background: '#FCEDDA',
+        primary: '#EE4E34',
+        modes: {
+            dark: {
+                text: '#fff',
+                background: '#000',
+                primary: '#0cf',
+            },
+        },
+    },
+};
